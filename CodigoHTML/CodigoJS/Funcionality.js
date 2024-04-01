2a6627eb1bf72dc99e2fa886af0c60480a2daefb
Raul Rodriguez <raulrodriguezalburquerque@gmail.com>
2024-04-01 13:14:55 +0200

    Cambiar de tema refinado

--- CodigoHTML/CodigoJS/Funcionality.js
+++ CodigoHTML/CodigoJS/Funcionality.js
@@ -1,12 +1,7 @@
 "use strict";
 
-// Contante con color gris oscuro
-const DARK_GREY = "#151E21";
-
 // Cuerpo
 var body = document.querySelector("body");
-// Textos del cuerpo
-var bodyTexts = document.getElementsByClassName("BodyText");
 
 // Botones para cambiar el tema de la pagina
 var themeChangers = document.querySelectorAll(".themeChanger");
@@ -17,11 +12,10 @@ themeChangers.forEach(button => {
 
 // Funcion para cambiar el tema de la pagina
 function themeChange() {
-    // Se cambia el color del fondo y textos del body
-    body.style.backgroundColor = DARK_GREY;
-    for (let text of bodyTexts) {
-        text.style.color = "white";
-    }
+    // Se cambia la clase del cuerpo a tema oscuro o se quita si ya la tenia
+    body.classList.toggle("DarkTheme");
+    // Se guarda si el cuerpo tiene el tema oscuro
+    storeTheme(body.classList.contains("DarkTheme"));
 }
 
 // Funcion para saludar al usuario con la fecha actual
@@ -30,7 +24,21 @@ function welcomeUser() {
     alert("¡Bienvenido! Estamos a "+date);
 }
 
-// Cuando la pagina se carga, se lanza un mensaje de bienvenida
+// Funcion para guardar si el tema oscuro esta seleccionado
+function storeTheme(theme) {
+    localStorage.setItem('darkTheme', theme);
+}
+
+// Cuando la pagina se carga, se carga el tema y se lanza un mensaje de bienvenida
 window.addEventListener("load", function(){
+    // Comprobamos el tema que teniamos puesto y se carga
+    const darkTheme = localStorage.getItem("darkTheme");
+    if(!darkTheme) {
+        storeTheme("false");
+    }
+    else if(darkTheme == "true") {
+        body.classList.add("DarkTheme");
+    }
+    // Lanzamos mensaje de bienvenida
     welcomeUser();
 });
